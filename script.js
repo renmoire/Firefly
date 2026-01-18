e6004ac55d42e5c98803428a197ebb5b4905465a
Ren <214003478+renmoire@users.noreply.github.com>
2026-01-18 16:36:18 +0700

    10% changes on section 1

---
+++ script.js
@@ -0,0 +1,8 @@
+const character = document.querySelector("#char")
+const voicebt = document.querySelector("#voicebt")
+
+console.log(character, voicebt)
+
+window.addEventListener("load", () => {
+    character.classList.add("fade")
+})
