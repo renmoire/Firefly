c851f9386faea5fe9ed233c1e5f7db360017139e
Ren <214003478+renmoire@users.noreply.github.com>
2026-01-19 15:43:34 +0700

    Adding section 2 with some slideshow animation init

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const character = document.querySelector("#char")
-const voicebt = document.querySelector("#voicebt")
+const character = document.querySelector("#char");
+const voicebt = document.querySelector("#voicebt");
+const slides = document.querySelectorAll(".frame");
+const scrolltext = document.querySelector(".scroll-indicator");
 
-console.log(character, voicebt)
+let current = 0;
 
 window.addEventListener("load", () => {
-    character.classList.add("fade")
-})
+    character.classList.add("fade");
+
+    setInterval(() => {
+    slides[current].classList.remove("active");
+    current = (current + 1) % slides.length;
+    slides[current].classList.add("active");
+    }, 3000)
+});
