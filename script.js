aced69949f94e41b3a8dcd42d5f5999c0860ea58
Ren <214003478+renmoire@users.noreply.github.com>
2026-01-20 17:34:36 +0700

    Adding related and some js to curate it

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const character = document.querySelector("#char");
 const voicebt = document.querySelector("#voicebt");
 const slides = document.querySelectorAll(".frame");
 const scrolltext = document.querySelector(".scroll-indicator");
+const cards = document.querySelectorAll(".cardind");
+const prevbt = document.getElementById("prevbtn");
+const nextbt = document.getElementById("nextbtn");
+const pageinfo = document.getElementById("pageinfo");
 
 let current = 0;
 
@@ -14,3 +18,39 @@ window.addEventListener("load", () => {
     slides[current].classList.add("active");
     }, 2500)
 });
+
+
+let currentpage = 1;
+const itemsperpage = 3;
+const totalpages = Math.ceil(cards.length / itemsperpage);
+
+function showpage(page) {
+    const start = (page - 1) * itemsperpage;
+    const end = start + itemsperpage;
+
+    cards.forEach((card, index) => {
+        card.style.display =
+        index >= start && index < end ? "flex" : "none";
+    });
+
+    pageinfo.textContent = `${page} / ${totalpages}`;
+    prevbt.disabled = page === 1;
+    nextbt.disabled = page === totalpages;
+}
+
+prevbt.addEventListener("click", () => {
+  if (currentpage > 1) {
+    currentpage--;
+    showpage(currentpage);
+  }
+});
+
+nextbt.addEventListener("click", () => {
+  if (currentpage < totalpages) {
+    currentpage++;
+    showpage(currentpage);
+  }
+});
+
+// init
+showpage(currentpage);
